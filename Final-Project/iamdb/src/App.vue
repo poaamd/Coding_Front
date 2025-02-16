922fc8f8f2dd4a5b763eb607ef874ad91c6ae447
Pouya Ahmadi <poa.amd@gmail.com>
2025-02-16 10:53:43 -0500

    Create Searchbar component along with css

--- Final-Project/iamdb/src/App.vue
+++ Final-Project/iamdb/src/App.vue
@@ -1,3 +1,15 @@
-<script setup></script>
+<script setup>
+import SearchBar from "@/components/SearchBar.vue";
+</script>
 
-<template></template>
+<template>
+  <SearchBar />
+</template>
+<style>
+* {
+  box-sizing: border-box;
+}
+body {
+  background: rgba(7, 13, 35, 1);
+}
+</style>
---
+++ Final-Project/iamdb/src/components/SearchBar.vue
@@ -0,0 +1,57 @@
+<script setup></script>
+
+<template>
+  <div class="search-bar">
+    <input class="text_area" type="text" />
+    <a href="#" title="Search"
+      ><img
+        src="@/assets/icons/search_icon.svg"
+        alt="Search icon"
+        class="search_icon"
+    /></a>
+    <a href="#" title="Use your voice for search">
+      <img src="@/assets/icons/mic_icon.svg" alt="Mic icon" class="mic_icon" />
+    </a>
+  </div>
+</template>
+<style scoped>
+.search-bar {
+  background: rgba(34, 44, 79, 0.95);
+  position: relative;
+  margin: 32px auto;
+  width: 920px;
+  height: 48px;
+  border-radius: 16px;
+}
+.text_area {
+  background: rgba(34, 44, 79, 0.95);
+  margin: 0 50px;
+  width: 88%;
+  height: 100%;
+  border: none;
+  outline: none;
+  border-radius: 16px;
+  color: aliceblue;
+  font-size: large;
+}
+.search_icon {
+  position: absolute;
+  top: 12px;
+  left: 16px;
+  width: 24px;
+}
+.search_icon:hover {
+  opacity: 0.5;
+}
+.mic_icon:hover {
+  opacity: 0.5;
+}
+
+.mic_icon {
+  position: absolute;
+  right: 12px;
+  top: 12px;
+  width: 40px;
+  height: 24px;
+}
+</style>

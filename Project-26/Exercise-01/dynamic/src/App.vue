ee88c1cd9f41632e221c9833d3c8fe39d194ae90
Pouya Ahmadi <poa.amd@gmail.com>
2025-02-19 15:17:37 -0500

    Fix emit binding to correctly pass item in congrats event.

--- Project-26/Exercise-01/dynamic/src/App.vue
+++ Project-26/Exercise-01/dynamic/src/App.vue
@@ -1,31 +1,67 @@
 <script setup>
 import Item from "@/components/Item.vue";
-const list = [
+import { ref } from "vue";
+// const list = [
+//   {
+//     id: 1,
+//     name: "John ",
+//     email: "John@mail.com",
+//   },
+//   {
+//     id: 2,
+//     name: "Max ",
+//     email: "Max@mail.com",
+//   },
+//   {
+//     id: 3,
+//     name: "Alex ",
+//     email: "Alex@mail.com",
+//   },
+// ];
+// const Jay = "Dynamic Jay";
+
+// Define a reactive list of items
+const list = ref([
   {
     id: 1,
     name: "John ",
-    email: "John@mail.com",
   },
-  {
-    id: 2,
-    name: "Max ",
-    email: "Max@mail.com",
-  },
-  {
-    id: 3,
-    name: "Alex ",
-    email: "Alex@mail.com",
-  },
-];
+]);
 
-const Jay = "Dynamic Jay";
+// Define a reactive variable for input name
+const name = ref("");
+
+// Function to add a new name to the list
+const addName = () => {
+  list.value.push({
+    id: 1,
+    name: name.value,
+  });
+  console.log(name.value);
+};
+
+// Function to show an alert when a "congrats" event is emitted
+const sayCongrats = (item) => {
+  alert(`Congrats ${item.name}`);
+};
 </script>
 
 <template>
   <div>App.vue</div>
-  <ul>
+  <!-- <ul>
     <Item v-for="item in list" :name="item.name" />
     <Item name="Pouya" />
     <Item :name="Jay" />
+  </ul> -->
+  <input type="text" v-model="name" />
+  <button @click="addName">Add</button>
+
+  <!-- Display the list of items using the Item component -->
+  <ul>
+    <Item
+      v-for="item in list"
+      :name="item.name"
+      @congrats="sayCongrats(item)"
+    />
   </ul>
 </template>
--- Project-26/Exercise-01/dynamic/src/components/Item.vue
+++ Project-26/Exercise-01/dynamic/src/components/Item.vue
@@ -1,7 +1,33 @@
 <script setup>
+// Define props to accept a "name" property from the parent component
 const props = defineProps(["name"]);
+
+// Define events that this component can emit
+const emit = defineEmits(["congrats"]);
+
+// Function to emit a "congrats" event when called
+const onCongrats = () => {
+  emit("congrats");
+};
+
+// Function to show an alert when "Remove" button is clicked
+const onRemove = () => {
+  alert("remove");
+};
+
+// Log the received name prop to the console for debugging purposes
 console.log(props.name);
 </script>
+
 <template>
-  <li>{{ name }}</li>
+  <li>
+    <!-- Display the name prop -->
+    {{ name }}
+
+    <!-- Button to trigger the "congrats" event -->
+    <button @click="onCongrats">Congrats</button>
+
+    <!-- Button to trigger the "onRemove" function -->
+    <button @click="onRemove">Remove</button>
+  </li>
 </template>
